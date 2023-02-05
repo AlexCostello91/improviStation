4d0aabe318e6563435513c988e81c6fbc079d290
Alex <alcostello91@gmail.com>
2023-02-05 17:41:46 -0500

    add devcontainer files

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo 'Hello World';
+echo 'Hello World'.PHP_EOL;
+try {
+    $dbh = new PDO("mysql:host=".gethostbyname('mariadb').";dbname=".getenv('DB_DATABASE'), getenv('DB_USER'), getenv('DB_PASSWORD'), [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "Connected to the database at hostname 'mariadb': " . gethostbyname('mariadb') . "\n".PHP_EOL;
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
 phpinfo();
