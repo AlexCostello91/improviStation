f524746bc627ddec3ee9b3da6319d392ac95d78d
Alex <alcostello91@gmail.com>
2023-02-15 23:17:43 +0000

    "finalize" devcontainer configs, continue with vue bootcamp

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+
+//Configure HTTPS for vite
+const sslOptions = {
+    key: fs.readFileSync('./docker/nginx/ssl/nginx.key'),
+    cert: fs.readFileSync('./docker/nginx/ssl/nginx.crt'),
+};
 
 export default defineConfig({
     plugins: [
@@ -17,4 +24,11 @@ export default defineConfig({
             },
         }),
     ],
+    server: { //Add options for docker dev config
+        https: sslOptions,
+        host: 'localhost',
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
