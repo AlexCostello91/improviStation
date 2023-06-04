ad6117397ee99ac74c6202df0bb9fea9a4fc8f42
Alex <alcostello91@gmail.com>
2023-06-04 16:33:43 +0000

    update homepage

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,11 +4,11 @@ import { ref } from 'vue'
 import { Head, Link } from '@inertiajs/vue3';
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import {
-    ArrowPathIcon,
+    ListBulletIcon,
     Bars3Icon,
-    CloudArrowUpIcon,
-    FingerPrintIcon,
-    LockClosedIcon,
+    ShareIcon,
+    ClockIcon,
+    ChartBarSquareIcon,
     XMarkIcon,
 } from '@heroicons/vue/24/outline'
 import { CheckIcon } from '@heroicons/vue/20/solid'
@@ -24,69 +24,28 @@ const navigation = [
 ]
 const features = [
     {
-        name: 'Push to deploy',
+        name: 'Macro tracking',
         description:
-            'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-        icon: CloudArrowUpIcon,
+            'See detailed breakdowns of all the macros you want to track. Calories, protein, fats, and more.',
+        icon: ChartBarSquareIcon,
     },
     {
-        name: 'SSL certificates',
+        name: 'Exercise Tracking',
         description:
-            'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-        icon: LockClosedIcon,
+            'From rock climbing to boogie boarding, keep track of burned calories and track your exercise goals.',
+        icon: ClockIcon,
     },
     {
-        name: 'Simple queues',
+        name: 'Meal Logging',
         description:
-            'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-        icon: ArrowPathIcon,
+            'Track everything you eat, and see what foods are standing between you and your goals.',
+        icon: ListBulletIcon,
     },
     {
-        name: 'Advanced security',
+        name: 'Meal Sharing',
         description:
-            'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-        icon: FingerPrintIcon,
-    },
-]
-const tiers = [
-    {
-        name: 'Freelancer',
-        id: 'tier-freelancer',
-        href: '#',
-        priceMonthly: '$24',
-        description: 'The essentials to provide your best work for clients.',
-        features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
-        mostPopular: false,
-    },
-    {
-        name: 'Startup',
-        id: 'tier-startup',
-        href: '#',
-        priceMonthly: '$32',
-        description: 'A plan that scales with your rapidly growing business.',
-        features: [
-            '25 products',
-            'Up to 10,000 subscribers',
-            'Advanced analytics',
-            '24-hour support response time',
-            'Marketing automations',
-        ],
-        mostPopular: true,
-    },
-    {
-        name: 'Enterprise',
-        id: 'tier-enterprise',
-        href: '#',
-        priceMonthly: '$48',
-        description: 'Dedicated support and infrastructure for your company.',
-        features: [
-            'Unlimited products',
-            'Unlimited subscribers',
-            'Advanced analytics',
-            '1-hour, dedicated support response time',
-            'Marketing automations',
-        ],
-        mostPopular: false,
+            'Search our database of user meals. Diets that work for others could also work for you!',
+        icon: ShareIcon,
     },
 ]
 const faqs = [
@@ -306,51 +265,6 @@ const mobileMenuOpen = ref(false)
                 </div>
             </div>
 
-            <!-- Pricing section -->
-            <div class="py-24 sm:pt-48">
-                <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div class="mx-auto max-w-4xl text-center">
-                        <h2 class="text-base font-semibold leading-7 text-indigo-600">Pricing</h2>
-                        <p class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Pricing plans for teams
-                            of&nbsp;all&nbsp;sizes</p>
-                    </div>
-                    <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">Distinctio et nulla eum
-                        soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id
-                        ut laborum.</p>
-                    <div
-                        class="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                        <div v-for="(tier, tierIdx) in tiers" :key="tier.id"
-                            :class="[tier.mostPopular ? 'lg:z-10 lg:rounded-b-none' : 'lg:mt-8', tierIdx === 0 ? 'lg:rounded-r-none' : '', tierIdx === tiers.length - 1 ? 'lg:rounded-l-none' : '', 'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10']">
-                            <div>
-                                <div class="flex items-center justify-between gap-x-4">
-                                    <h3 :id="tier.id"
-                                        :class="[tier.mostPopular ? 'text-indigo-600' : 'text-gray-900', 'text-lg font-semibold leading-8']">
-                                        {{ tier.name }}</h3>
-                                    <p v-if="tier.mostPopular"
-                                        class="rounded-full bg-indigo-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-indigo-600">
-                                        Most popular</p>
-                                </div>
-                                <p class="mt-4 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
-                                <p class="mt-6 flex items-baseline gap-x-1">
-                                    <span class="text-4xl font-bold tracking-tight text-gray-900">{{ tier.priceMonthly
-                                    }}</span>
-                                    <span class="text-sm font-semibold leading-6 text-gray-600">/month</span>
-                                </p>
-                                <ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-600">
-                                    <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
-                                        <CheckIcon class="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                                        {{ feature }}
-                                    </li>
-                                </ul>
-                            </div>
-                            <a :href="tier.href" :aria-describedby="tier.id"
-                                :class="[tier.mostPopular ? 'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500' : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300', 'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']">Buy
-                                plan</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <!-- FAQs -->
             <div
                 class="mx-auto max-w-2xl divide-y divide-gray-900/10 px-6 pb-8 sm:pb-24 sm:pt-12 lg:max-w-7xl lg:px-8 lg:pb-32">
@@ -363,33 +277,6 @@ const mobileMenuOpen = ref(false)
                         </dd>
                     </div>
                 </dl>
-            </div>
-
-            <!-- CTA section -->
-            <div class="relative -z-10 mt-32 px-6 lg:px-8">
-                <div class="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 transform-gpu justify-center overflow-hidden blur-3xl sm:bottom-0 sm:right-[calc(50%-6rem)] sm:top-auto sm:translate-y-0 sm:transform-gpu sm:justify-end"
-                    aria-hidden="true">
-                    <div class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-25"
-                        style="clip-path: polygon(73.6% 48.6%, 91.7% 88.5%, 100% 53.9%, 97.4% 18.1%, 92.5% 15.4%, 75.7% 36.3%, 55.3% 52.8%, 46.5% 50.9%, 45% 37.4%, 50.3% 13.1%, 21.3% 36.2%, 0.1% 0.1%, 5.4% 49.1%, 21.4% 36.4%, 58.9% 100%, 73.6% 48.6%)" />
-                </div>
-                <div class="mx-auto max-w-2xl text-center">
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Boost your
-                        productivity.<br />Start using our app today.</h2>
-                    <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">Incididunt sint fugiat pariatur
-                        cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
-                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                            started</a>
-                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
-                                aria-hidden="true">→</span></a>
-                    </div>
-                </div>
-                <div class="absolute left-1/2 right-0 top-full -z-10 hidden -translate-y-1/2 transform-gpu overflow-hidden blur-3xl sm:block"
-                    aria-hidden="true">
-                    <div class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-                        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-                </div>
             </div>
         </main>
 
